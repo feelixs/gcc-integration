e5c9bb6bb2cf44af666c83beed16449fbbd0ad5b
Michael Felix <mikemh@uri.edu>
2025-04-15 09:50:49 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,21 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.7.20"
-    id("org.jetbrains.intellij") version "1.13.3"
+    id("org.jetbrains.kotlin.jvm") version "1.9.22"
+    id("org.jetbrains.intellij.platform") version "2.5.0"
 }
 
 group = "com.mikefmh.gcc-integration"
 
 // WHEN UPLOADING A NEW VERSION:
 // edit this file to update compatibility -> it will update xml in build
+// make sure 'plugins {' is set to the updated versions
 version = "1.2.5"
 
 repositories {
     mavenCentral()
 }
 
-intellij {
+intellijPlatform {
     type.set("IC")
     version.set("2024.3.5")
     updateSinceUntilBuild.set(true)
