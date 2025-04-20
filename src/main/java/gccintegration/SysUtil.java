da50670ffacb6b89e983ae046c2c0294329a417f
Michael Felix <michael@felixcreations.com>
2025-04-20 09:43:47 -0400

    Update SysUtil.java

--- src/main/java/gccintegration/SysUtil.java
+++ src/main/java/gccintegration/SysUtil.java
@@ -27,6 +27,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import com.intellij.openapi.util.Key;
 import java.util.ArrayList;
 import java.util.List;
